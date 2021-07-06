c44ac2b9f423aa0d7ad9926eaf583800eed63cdf
HarryPotter12352 <79324017+HarryPotter12352@users.noreply.github.com>
2021-07-06 21:09:51 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -22,6 +22,7 @@ class Moderation(commands.Cog):
                 title="Banned", description=f"You have been banned from **{ctx.guild.name} by **{ctx.author}** for reason **{reason}**", colour=ctx.author.colour, timestamp = datetime.datetime.now())
             embed.set_thumbnail(url=ctx.author.avatar_url)
             await member.send(embed=embed)
+            await ctx.send(f"Successfully banned {member}!")
 
 
         @ban.error 
@@ -46,3 +47,47 @@ class Moderation(commands.Cog):
             embed = discord.Embed(title="Kicked", description=f'You have been kicked from **{ctx.guild.name}** by **{ctx.author}** for reason **{reason}**', timestamp = datetime.datetime.now(), colour = ctx.author.colour)
             embed.set_thumbnail(url=ctx.author.avatar_url)
             await member.send(embed=embed)
+            await ctx.send(f"Successfully kicked {member}!")
+
+        @kick.error 
+        async def ban_error(ctx, error):
+            if isinstance(error, commands.MissingPermissions):
+                await ctx.send(f"You need the `kick_members` permissions required to run this command {ctx.author.mention}!")
+            if isinstance(error, commands.MissingRequiredArgument):
+                await ctx.send("Please provide all of the vaild parameters. They are `member` and `reason`")
+            if isinstance(error, commands.BotMissingPermissions):
+                await ctx.send("I do not have the needed permissions! They are `Kick members`")
+            if isinstance(error, commands.MemberNotFound):
+                await ctx.send("They're a :ghost: How do I interact with them?")
+
+
+        @commands.command()
+        @commands.has_permissions(manage_messages=True)
+        @commands.bot_has_permissions(manage_roles=True)
+        async def mute(ctx, member : discord.Member, *, reason : str):
+            """
+            Mutes a member from sending any messages
+            """
+            mutedRole = discord.utils.get(ctx.guild.roles, name = "Muted")
+            if not mutedRole:
+                channels = 0
+                mutedRole = await ctx.guild.create_role(name="Muted")
+                for channel in ctx.guild.text_channels:
+                    await channel.set_permissions(mutedRole, send_messages=False)
+                    channels += 1 
+                await ctx.send(f"Successfully applied overwrites for {channels} channels")
+            await member.add_roles(mutedRole)
+            embed = discord.Embed(title="Muted", description = f"You have been muted in **{ctx.guild.name}** by **{ctx.author}** **indefinetly** for reason **{reason}**", colour = ctx.author.color, timestamp = datetime.datetime.now())
+            await member.send(embed=embed)
+
+
+        @mute.error 
+        async def mute_error(ctx, error):
+            if isinstance(error, commands.MissingPermissions):
+                await ctx.send(f"You need the `manage_messages` permissions required to run this command {ctx.author.mention}!")
+            if isinstance(error, commands.MissingRequiredArgument):
+                await ctx.send("Please provide all of the vaild parameters. They are `member` and `reason`")
+            if isinstance(error, commands.BotMissingPermissions):
+                await ctx.send("I do not have the needed permissions! They are `Manage roles`")
+            if isinstance(error, commands.MemberNotFound):
+                await ctx.send("They're a :ghost: How do I interact with them?")
