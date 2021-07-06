971e0e83c99c74c4e6607caf1a5f7327b9e5f1f2
HarryPotter12352 <79324017+HarryPotter12352@users.noreply.github.com>
2021-07-06 16:20:45 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -1,20 +1,36 @@
 import discord
-from discord.ext import commands 
-import asyncio 
-import datetime 
-
+from discord.ext import commands
+import asyncio
+import datetime
 
 
 class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-
         @commands.command()
-        async def ban(ctx, member : discord.Member, *, reason : str):
+        @commands.has_permissions(ban_members=True)
+        @commands.bot_has_permissions(ban_members=True)
+        async def ban(ctx, member: discord.Member, *, reason: str):
             """
             Bans a member from a server
             Very used command
             """
             await member.ban(reason=reason)
-            
+            embed = discord.Embed(
+                title="Banned", description=f"You have been banned from **{ctx.guild.name} by **{ctx.author}** for reason **{reason}**", colour=ctx.author.colour, timestamp = datetime.datetime.now())
+            embed.set_thumbnail(url=ctx.author.avatar_url)
+            await member.send(embed=embed)
+
+
+        @ban.error 
+        async def ban_error(ctx, error):
+            if isinstance(error, commands.MissingPermissions):
+                await ctx.send(f"You need the `ban_members` permissions required to run this command {ctx.author.mention}!")
+            if isinstance(error, commands.MissingRequiredArgument):
+                await ctx.send("Please provide all of the vaild parameters. They are `member` and `reason`")
+            if isinstance(error, commands.BotMissingPermissions):
+                await ctx.send("I do not have the needed permissions! They are `Ban members`")
+            if isinstance(error, commands.MemberNotFound):
+                await ctx.send("They're a :ghost: How do I interact with them?")
+                
