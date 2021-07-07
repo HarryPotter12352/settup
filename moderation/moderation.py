b630a43334b91ff3c6e8e60d96463dbac6fc2e7d
HarryPotter12352 <79324017+HarryPotter12352@users.noreply.github.com>
2021-07-07 10:46:39 +0530

    Update moderation.py
    Tempmute command added

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -12,7 +12,7 @@ class Moderation(commands.Cog):
         @commands.command()
         @commands.has_permissions(ban_members=True)
         @commands.bot_has_permissions(ban_members=True)
-        async def ban(self, ctx, member: discord.Member, *, reason: str):
+        async def ban(ctx, member: discord.Member, *, reason: str):
             """
             Bans a member from a server
             Very used command
@@ -26,7 +26,7 @@ class Moderation(commands.Cog):
 
 
         @ban.error 
-        async def ban_error(self, ctx, error):
+        async def ban_error(ctx, error):
             if isinstance(error, commands.MissingPermissions):
                 await ctx.send(f"You need the `ban_members` permissions required to run this command {ctx.author.mention}!")
             if isinstance(error, commands.MissingRequiredArgument):
@@ -39,7 +39,7 @@ class Moderation(commands.Cog):
         @commands.command()
         @commands.has_permissions(kick_members=True)
         @commands.bot_has_permissions(kick_members=True)
-        async def kick(self, ctx, member : discord.Member, *, reason : str):
+        async def kick(ctx, member : discord.Member, *, reason : str):
             """
             Kicks a member
             """
@@ -50,7 +50,7 @@ class Moderation(commands.Cog):
             await ctx.send(f"Successfully kicked {member}!")
 
         @kick.error 
-        async def ban_error(self, ctx, error):
+        async def ban_error(ctx, error):
             if isinstance(error, commands.MissingPermissions):
                 await ctx.send(f"You need the `kick_members` permissions required to run this command {ctx.author.mention}!")
             if isinstance(error, commands.MissingRequiredArgument):
@@ -64,7 +64,7 @@ class Moderation(commands.Cog):
         @commands.command()
         @commands.has_permissions(manage_messages=True)
         @commands.bot_has_permissions(manage_roles=True)
-        async def mute(self, ctx, member : discord.Member, *, reason : str):
+        async def mute(ctx, member : discord.Member, *, reason : str):
             """
             Mutes a member from sending any messages
             """
@@ -82,7 +82,7 @@ class Moderation(commands.Cog):
 
 
         @mute.error 
-        async def mute_error(self, ctx, error):
+        async def mute_error(ctx, error):
             if isinstance(error, commands.MissingPermissions):
                 await ctx.send(f"You need the `manage_messages` permissions required to run this command {ctx.author.mention}!")
             if isinstance(error, commands.MissingRequiredArgument):
@@ -91,32 +91,54 @@ class Moderation(commands.Cog):
                 await ctx.send("I do not have the needed permissions! They are `Manage roles`")
             if isinstance(error, commands.MemberNotFound):
                 await ctx.send("They're a :ghost: How do I interact with them?")
-                
-                
+
+        def convert(time):
+            pos = ["s", "m", "h", "d"]
+
+            time_dict = {"s" : 1, "m" : 60, "h" : 3600, "d" : 3600*24, "w" : 3600*24*7}
+
+            unit = time[-1]
+
+            if unit not in pos:
+                return -1
+
+            try:
+                val = int(time[:-1])
+            except:
+                return -2
+
+
+            return val * time_dict[unit]
+
         @commands.command()
         @commands.has_permissions(manage_messages=True)
         @commands.bot_has_permissions(manage_roles=True)
-        async def unmute(self, ctx, member : discord.Member):
+        async def tempmute(ctx, member : discord.Member, time, *, reason : str):
+            converted_time = convert(time)
             mutedRole = discord.utils.get(ctx.guild.roles, name = "Muted")
             if not mutedRole:
                 mutedRole = await ctx.guild.create_role(name='Muted')
-                for channel in ctx.guild.channels:
-                    await channel.set_overwrites(mutedRole, send_messages=False)
+                for channel in ctx.guild.text_channels:
+                    await channel.set_permissions(mutedRole, send_messages=False)
+            await member.add_roles(mutedRole)
+            embed = discord.Embed(title="Muted", description = f"You have been muted from **{ctx.guild.name}** by **{ctx.author}** for **{time}** for reason **{reason}**", colour = ctx.author.color, timestamp = datetime.datetime.now())
+            await member.send(embed=embed)
+            await ctx.send(f"Successfully muted {member} for {time}!")
+            await asyncio.sleep(converted_time)
             if mutedRole in member.roles:
                 await member.remove_roles(mutedRole)
-                embed = discord.Embed(title="Unmuted", description = f"You have been unmuted from **{ctx.guild.name}** by **{ctx.author}**!", colour = ctx.author.colour, timestamp = datetime.datetime.now())
-                await member.send(embed=embed)
-                await ctx.send(f"{member} has successfully been unmuted!")
+                embed = discord.Embed(title="Unmuted!", description = f"You have now been unmuted from **{ctx.guild.name}** after a tempmute of **{time}**")
             else:
-                await ctx.send("They're not muted bruh")
+                return False
 
-        @unmute.error 
-        async def unmute_error(self, ctx, error):
+
+        @tempmute.error 
+        async def tempmute_error(ctx, error):
             if isinstance(error, commands.MissingPermissions):
-                await ctx.send(f"You need the `manage messages` permission!")
+                await ctx.send(f"You need the `manage_messages` permissions required to run this command {ctx.author.mention}!")
             if isinstance(error, commands.MissingRequiredArgument):
-                await ctx.send("Please provide all arguments! They are `member`")
+                await ctx.send("Please provide all of the vaild parameters. They are `member`, `time` and `reason`")
             if isinstance(error, commands.BotMissingPermissions):
-                await ctx.send("I need the `manage roles` permission!")
+                await ctx.send("I do not have the needed permissions! They are `Manage roles`")
             if isinstance(error, commands.MemberNotFound):
-                await ctx.send(f"How do I interact with a :ghost:")
+                await ctx.send("They're a :ghost: How do I interact with them?")
