4b2eabe7866107867d1dc228bc230bb1e61eaa16
Aryaman Srivastava <aryamansrivastava2006@gmail.com>
2021-07-07 16:23:48 +0530

    Create quote.py

---
+++ quotes/quote.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from utils.decorators import command, cooldown
+import aiohttp
+import json
+import decimal
+
+
+class Quote(commands.Cog):
+  def __init__(self,client):
+    self.client = client
+
+  @command("quote")  
+  async def q(self,ctx):
+    async with aiohttp.ClientSession() as r:
+      async with r.get("https://api.quotable.io/random") as r:
+        data = await r.json()
+
+        embed = discord.Embed(title = "Quote for you", description = f'{data["content"]}', colour = discord.Colour.blue()) 
+        embed.set_thumbnail(url=' ')
+   
+    await ctx.send(embed = embed)
+
+
+def setup(client):
+    client.add_cog(Quote(client))
