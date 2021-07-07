ab2ccce539afdda6b2287bcfaf9a96783f618645
HarryPotter12352 <79324017+HarryPotter12352@users.noreply.github.com>
2021-07-07 10:54:06 +0530

    Update devtools.py
    Added grab image function

--- devtools/devtools.py
+++ devtools/devtools.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from flask import Flask
 from threading import Thread
+import requests
+import datetime
 
 
 
@@ -11,7 +13,7 @@ def keep_alive():
     """
     This will host your bot for 1 hour
     To continue with the hosting, you have to link it up with an uptime service
-    Note: This will work only on replit
+    Note: This will work on on replit
     """
     app = Flask("")
     @app.route("/")
@@ -24,8 +26,7 @@ def keep_alive():
 
     server = Thread(target=run)
     server.start()
-    
-  
+
 
 def meme():
     """
@@ -41,4 +42,15 @@ def meme():
     embed = discord.Embed(title=title,description=f"By **{author}**", color=0xFF5733)
     embed.set_image(url=img)
     embed.set_footer(text = f"{upvote} upvotes")
+    return embed
+
+
+def getimg(topic):
+    """
+    This feature allows you to easily grab images from anywhere
+    """
+    topic = str(topic)
+    embed = discord.Embed(title=f"Image on {topic}!", colour = discord.Colour.random(), timestamp = datetime.datetime.now())
+    url = f"https://source.unsplash.com/900x/900/?{topic}"
+    embed.set_image(url=url)
     return embed
