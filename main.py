a06a2779351c2d1dddd4a02cf2ff497d0e4f1e52
Aryaman Srivastava <aryamansrivastava2006@gmail.com>
2021-07-08 18:08:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ class NewHelpName(commands.MinimalHelpCommand):
         destination = self.get_destination()
         for page in self.paginator.pages:
             emby = discord.Embed(description=page, colour = discord.Colour.red(), title = ":bookmark_tabs: | Help Menu")
-            emby.set_thumbnail(url = "https://cdn.discordapp.com/avatars/566193564502196235/b624ea7737776938c070f6693c91abc9?size=2048")
-            emby.set_footer(text = "Check Bot Status at https://RKS.aryamansri.repl.co ")
+            emby.set_thumbnail(url = " ")
             await destination.send(embed=emby)
 
 client.help_command = NewHelpName()
