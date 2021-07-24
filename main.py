d4b3f01fcb17a0091a77c758e13b19e18e646759
HarryPotter12352 <79324017+HarryPotter12352@users.noreply.github.com>
2021-07-24 15:35:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+from datetime import datetime
 import discord
 from discord.ext import commands
 
 
+
 def init(prefix):
     prefix = str(prefix)
-    client = commands.Bot(command_prefix=prefix, help_command=NewHelpName)
+    client = commands.Bot(command_prefix=prefix)
     return client
 
 
-class NewHelpName(commands.MinimalHelpCommand):
-    async def send_pages(self):
-        destination = self.get_destination()
-        for page in self.paginator.pages:
-            emby = discord.Embed(description=page, colour = discord.Colour.red(), title = ":bookmark_tabs: | Help Menu")
-            emby.set_thumbnail(url = " ")
-            await destination.send(embed=emby)
+def help(client : commands.Bot):
+    commands = client.commands 
+    embed = discord.Embed(title="List of all commands", colour = discord.Colour.random(), timestamp=datetime.now())
+    for command in commands:
+        embed.add_field(name=command.name, value = command.description)
+        return embed
